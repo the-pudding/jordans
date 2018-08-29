1f0482fc5b69fc3672cedc5e292361b83b276d9f
russellgoldenberg <russellgoldenberg@gmail.com>
2018-08-29 14:36:45 -0400

    playin wit flubba

--- src/js/graphic.js
+++ src/js/graphic.js
@@ -1,122 +1,259 @@
 /* global d3 */
-import gsap from 'gsap';
-import morphSVG from './MorphSVGPlugin';
-//import interpolate from 'flubber';
+// import gsap from 'gsap';
+import * as flubber from 'flubber';
+import tracker from './utils/tracker';
+// import morphSVG from './MorphSVGPlugin';
 
 function resize() {}
 
 function gsapAnimation() {
-	var tl = new TimelineMax({paused:false})
+	const tl = new TimelineMax({ paused: false });
 
-	tl.to("#j1_x5F_tongueRed", 1, {
-		morphSVG: {shape:"#j2_x5F_tongueBlack"},
-		fill: '#000'
-	}, 0)
-	.to("#j1_x5F_topBlack", 1, {
-		morphSVG: {shape:"#j2_x5F_topBlack"}
-	}, 0)
-	.to("#j1_x5F_ankleRed", 1, {
-		morphSVG: {shape:"#j2_x5F_ankleWhite"},
-		fill: '#ffffff'
-	}, 0)
-	.to("#j1_x5F_midWhite", 1, {
-		morphSVG: {shape:"#j2_x5F_midWhite"}
-	}, 0)
-	.to("#j1_x5F_lacesWhite", 1, {
-		morphSVG: {shape:"#j2_x5F_tongueWhite"}
-	}, 0)
-	.to("#j1_x5F_toeWhite", 1, {
-		morphSVG: {shape:"#j2_x5F_bodyWhite"}
-	}, 0)
-	.to("#j1_x5F_toeRed", 1, {
-		morphSVG: {shape:"#j2_x5F_frontToeWhite"},
-		fill: '#ffffff'
-	}, 0)
-	.to("#j1_x5F_lacesRed", 1, {
-		morphSVG: {shape:"#j2_x5F_redOutline"},
-		fill: '#b42c30'
-	}, 0)
-	.to("#j1_x5F_ankleBlack", 1, {
-		morphSVG: {shape:"#j2_x5F_blackOutline"}
-	}, 0)
-	.to("#j1_x5F_nikeBlack", 1, {
-		morphSVG: {shape:"#j2_x5F_waveRed"},
-		fill: '#b42c30'
-	}, 0)
-	.to("#j1_x5F_backRed", 1, {
-		morphSVG: {shape:"#j2_x5F_midRed"}
-	}, 0)
-	.to("#j1_x5F_baseWhite", 1, {
-		morphSVG: {shape:"#j2_x5F_backWhite"}
-	}, 0)
-	.to("#j1_x5F_supportRed", 1, {
-		morphSVG: {shape:"#j2_x5F_soleBlack"},
-		fill: '#000'
-	}, 0)
-	.to("#j1_x5F_baseRed", 1, {
-		morphSVG: {shape:"#j2_x5F_soleRed"}
-	}, 0)
-	tl.to("#jordan2", 0.25, {
+	tl.to(
+		'#j1_x5F_tongueRed',
+		1,
+		{
+			morphSVG: { shape: '#j2_x5F_tongueBlack' },
+			fill: '#000'
+		},
+		0
+	)
+		.to(
+			'#j1_x5F_topBlack',
+			1,
+			{
+				morphSVG: { shape: '#j2_x5F_topBlack' }
+			},
+			0
+		)
+		.to(
+			'#j1_x5F_ankleRed',
+			1,
+			{
+				morphSVG: { shape: '#j2_x5F_ankleWhite' },
+				fill: '#ffffff'
+			},
+			0
+		)
+		.to(
+			'#j1_x5F_midWhite',
+			1,
+			{
+				morphSVG: { shape: '#j2_x5F_midWhite' }
+			},
+			0
+		)
+		.to(
+			'#j1_x5F_lacesWhite',
+			1,
+			{
+				morphSVG: { shape: '#j2_x5F_tongueWhite' }
+			},
+			0
+		)
+		.to(
+			'#j1_x5F_toeWhite',
+			1,
+			{
+				morphSVG: { shape: '#j2_x5F_bodyWhite' }
+			},
+			0
+		)
+		.to(
+			'#j1_x5F_toeRed',
+			1,
+			{
+				morphSVG: { shape: '#j2_x5F_frontToeWhite' },
+				fill: '#ffffff'
+			},
+			0
+		)
+		.to(
+			'#j1_x5F_lacesRed',
+			1,
+			{
+				morphSVG: { shape: '#j2_x5F_redOutline' },
+				fill: '#b42c30'
+			},
+			0
+		)
+		.to(
+			'#j1_x5F_ankleBlack',
+			1,
+			{
+				morphSVG: { shape: '#j2_x5F_blackOutline' }
+			},
+			0
+		)
+		.to(
+			'#j1_x5F_nikeBlack',
+			1,
+			{
+				morphSVG: { shape: '#j2_x5F_waveRed' },
+				fill: '#b42c30'
+			},
+			0
+		)
+		.to(
+			'#j1_x5F_backRed',
+			1,
+			{
+				morphSVG: { shape: '#j2_x5F_midRed' }
+			},
+			0
+		)
+		.to(
+			'#j1_x5F_baseWhite',
+			1,
+			{
+				morphSVG: { shape: '#j2_x5F_backWhite' }
+			},
+			0
+		)
+		.to(
+			'#j1_x5F_supportRed',
+			1,
+			{
+				morphSVG: { shape: '#j2_x5F_soleBlack' },
+				fill: '#000'
+			},
+			0
+		)
+		.to(
+			'#j1_x5F_baseRed',
+			1,
+			{
+				morphSVG: { shape: '#j2_x5F_soleRed' }
+			},
+			0
+		);
+	tl.to('#jordan2', 0.25, {
 		visibility: 'visible'
-	})
+	});
 }
 
 function flubberSingle() {
-	const test1 = d3.select('#j1_x5F_1').attr('d')
-	const test2 = d3.select('#j2_x5F_1').attr('d')
+	const test1 = d3.select('#j1_x5F_1').attr('d');
+	const test2 = d3.select('#j2_x5F_1').attr('d');
+	console.log({ test1 });
 
 	const interpolator = flubber.interpolate(test1, test2);
 
-	d3.select("#j1_x5F_1")
+	d3.select('#j1_x5F_1')
 		.transition()
 		.duration(1000)
-		.attrTween("d", function(){ return interpolator; });
+		.attrTween('d', () => interpolator);
 }
 
 function flubberArray() {
-	const test1 = d3.select('#j1_x5F_1').attr('d')
-	const test2 = d3.select('#j2_x5F_1').attr('d')
-	const test3 = d3.select('#j3_x5F_1').attr('d')
-	const test4 = d3.select('#j4_x5F_1').attr('d')
+	const test1 = d3.select('#j1_x5F_1').attr('d');
+	const test2 = d3.select('#j2_x5F_1').attr('d');
+	const test3 = d3.select('#j3_x5F_1').attr('d');
+	const test4 = d3.select('#j4_x5F_1').attr('d');
 
 	// Remove all the paths except the first
-	d3.selectAll("#jordan2").remove();
-	d3.selectAll("#jordan3").remove();
-	d3.selectAll("#jordan4").remove();
+	d3.selectAll('#jordan2').remove();
+	d3.selectAll('#jordan3').remove();
+	d3.selectAll('#jordan4').remove();
 
-	let shapeArray = []
-	shapeArray.push(test1, test2, test2, test4)
+	const shapeArray = [];
+	shapeArray.push(test1, test2, test2, test4);
 
-	console.log(shapeArray)
+	console.log(shapeArray);
 
-	//const interpolator = flubber.interpolate(test1, test2);
+	// const interpolator = flubber.interpolate(test1, test2);
 
 	d3.select('#j1_x5F_1')
 		.style('display', 'block')
-    .call(animate)
+		.call(animate);
 
 	function animate(sel) {
-		let start = shapeArray.shift(),
-				end = shapeArray[0]
+		const start = shapeArray.shift();
+
+		const end = shapeArray[0];
 
 		shapeArray.push(start);
 
 		sel
-			.datum({start, end})
+			.datum({ start, end })
 			.transition()
 			.duration(1500)
-	    .attrTween("d", function(d){
-				return flubber.interpolate(d.start, d.end, { maxSegmentLength: 0.1})
-			})
-			.on("end", function() {
-      	sel.call(animate);
-    	});
+			.attrTween('d', d =>
+				flubber.interpolate(d.start, d.end, { maxSegmentLength: 0.1 })
+			)
+			.on('end', () => {
+				sel.call(animate);
+			});
 	}
 }
 
+function test(index) {
+	const output = [];
+	// selects specific jordan
+	d3.select(`#jordan${index + 1}`)
+		.selectAll('path')
+		.each((d, i, n) => {
+			const $path = d3.select(n[i]);
+			const pathCoordinates = $path.at('d');
+			output.push(pathCoordinates);
+		});
+	// 	.each(function(d,i,n) {
+	// 	const $path = d3.select(this)
+	// 	const pathCoordinates = $path.at('d')
+	// 	output.push(pathCoordinates);
+	// });
+	return output;
+}
+
+function getPaths(index) {
+	const output = [];
+	d3.select(`#jordan${index}`)
+		.selectAll('path')
+		.each((d, i, n) => {
+			const $path = d3.select(n[i]);
+			const pathCoordinates = $path.at('d');
+			console.log($path.at('id'));
+			output.push(pathCoordinates);
+		});
+	return output;
+}
+
+function russell() {
+	const j1 = getPaths(1)
+		.reverse()
+		.slice(0, 13);
+	const j2 = getPaths(2)
+		.reverse()
+		.slice(0, 13);
+	// console.log({ j1, j2 });
+	let interpolator = null;
+	if (j1.length === j2.length) {
+		interpolator = flubber.interpolateAll(j1, j2, {
+			match: true,
+			single: false
+		});
+	} else if (j1.length < j2.length) {
+		interpolator = flubber.separate(j1, j2, { single: false });
+	} else {
+		interpolator = flubber.combine(j1, j2, { single: false });
+	}
+	// console.log({ interpolator });
+	d3.select('#jordan1')
+		.selectAll('path')
+		.data(interpolator)
+		.transition()
+		.duration(1500)
+		.attrTween('d', d => d);
+	// .on('end', () => {
+	// 	sel.call(animate);
+	// });
+}
+
 function init() {
-	//flubberArray()
-	flubberSingle()
+	russell();
+	// flubberArray()
+	// flubberSingle();
+	// window.output = JSON.stringify(d3.range(4).map(test));
 }
 
 export default { init, resize };
